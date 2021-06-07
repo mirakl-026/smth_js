39e12437c2538a651d95d4d6960556a97ed9497c
mirakl-026 <evgeniy.pgm@inbox.ru>
2021-06-07 19:51:21 +0300

    циклы

---
+++ 2_base/cycles.js
@@ -0,0 +1,42 @@
+// циклы
+
+var numbers = [1,2,3,4,5,6]
+
+numbers.push('bleh')
+numbers.unshift(10)
+
+for (var i = 0; i < numbers.length; i++) {
+    console.log(numbers[i])
+}
+
+// чётные
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+        console.log(numbers[i])
+    }    
+}
+
+// тоже чётные
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 !== 0) {
+        continue
+    }    
+    console.log(numbers[i])
+}
+
+// нечётные
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 !== 0) {
+        console.log(numbers[i])
+    }   
+}
+
+numbers.push(11)
+
+// просто
+for (let i = 0; i < numbers.length; i++) {
+    if (typeof numbers[i] === 'string') {
+        break
+    }   
+    console.log(numbers[i])
+}
