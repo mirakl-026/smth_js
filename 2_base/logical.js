f7c516562f016025690328f43936ef839819cddd
mirakl-026 <evgeniy.pgm@inbox.ru>
2021-06-07 17:20:04 +0300

    логические операторы

---
+++ 2_base/logical.js
@@ -0,0 +1,30 @@
+// логические операторы
+
+/*
+
+    && | true  | false
+  true | true  | false 
+ false | false | false   
+
+    || | true  | false
+  true | true  | true 
+ false | true  | false   
+
+  && (и) - true если все значения true
+  || (или) - true если хотя бы одно значение true
+  !(нет) - инвертирует true или false
+
+*/
+
+console.log('true && true:', true && true)
+console.log('true && false:',true && false)
+console.log('true || false:', true || false)
+console.log('false || false:', false || false)
+console.log('false || !false:', false || !false)
+
+console.log('(false && true || (true && false) || !true):', ((false && true) || (true || false) || !true))
+
+// console.log(true | false)
+// console.log(true & false)
+// console.log(true ^ false)
+// console.log(true ^ true)
