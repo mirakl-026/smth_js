305eae1ef4ea6d89b8ce129823facc381c5f7ca2
mirakl-026 <evgeniy.pgm@inbox.ru>
2021-06-07 14:52:36 +0300

    переменые и соглашения об именованиях

---
+++ 2_base/script.js
@@ -0,0 +1,28 @@
+console.log('Just arg');
+
+var message = "I'm variable"; // <-переменная
+var camelMessage = "camelMessage!"; // camesCase предпочтительно, но можно змеиным регистром
+var snake_message = 'snake_message!'; // змеиный регистр
+
+var $lol = 'Dollar';
+var _kek = 'underscore';
+
+console.log(message);
+
+/*
+    Соглашение об именованиях:
+
+    1. Инглиш офк
+    2. Лушче camelCase но если очень хочется то можно snake_case
+    3. используемые символы: $ _ цифры после остальных 
+    4. Регистрозависимый
+    5. Точка с запятой возможна, но не обязательна
+        - при отсутствии возможны баги
+        - но если по гайдам - можно и без
+    
+    6. Строки - можно использовать '', ""
+        - принято использовать одинарные кавычки
+*/
+
+var messageString2 = 'message 2'
+var messageString3 = "message 3"
