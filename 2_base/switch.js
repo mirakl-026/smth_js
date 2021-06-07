0728aff5ea694b7958e9cc6c8741eff6b8a207f3
mirakl-026 <evgeniy.pgm@inbox.ru>
2021-06-07 17:57:18 +0300

    switch case

---
+++ 2_base/switch.js
@@ -0,0 +1,29 @@
+// switch case
+
+var carColor = 'red'
+
+if (carColor === 'red'){
+    console.log('Цвет машины красный')
+} else if (carColor === 'yellow') {
+    console.log('Цвет машины жёлтый') 
+} else if (carColor === 'green') {
+    console.log('Цвет машины зелёный')
+} else {
+    console.log('Цвет машины какой-то')
+}
+
+
+carColor = 'green'
+switch (carColor) {
+    case 'red':
+        console.log('Цвет машины красный')
+        break
+    case 'green':
+        console.log('Цвет машины зелёный')
+        break
+    case 'yellow':
+        console.log('Цвет машины жёлтый')
+        break
+    default:
+        console.log('Цвет машины какой-то')
+}
