133c48cbb8138559871c854faff00c72b40a3a50
Evgeniy Bushin <evgeniy.pgm@inbox.ru>
2021-06-08 13:58:59 +0300

    объект Math

---
+++ 4_additional/4_math.js
@@ -0,0 +1,13 @@
+// объект math
+
+console.log(Math.random())
+
+var num = 2.4
+console.log(Math.floor(num))    // округление вниз
+console.log(Math.ceil(num))     // округление вверх
+
+console.log(Math.PI)
+console.log(Math.E)
+
+console.log(Math.Pi * 2 * 5)    // длина окружности радиусом 5
+
