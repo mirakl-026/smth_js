2c4c2024f2843a37bc1ecd2a3fb26ac1a39baa56
Evgeniy Bushin <evgeniy.pgm@inbox.ru>
2021-06-08 12:39:20 +0300

    всплытие и погружение обработчиков событий

---
+++ 3_dom/7_up_down.js
@@ -0,0 +1,18 @@
+// события
+
+var divs = document.querySelectorAll('div')
+
+// от текущего к родителю
+for (var i = 0; i < divs.length; i++){
+    divs[i].addEventListener('click', function() {
+        console.log(this.getAttribute('id'))    
+    }) // на погружение
+}
+
+
+// от родителя к текущему
+// for (var i = 0; i < divs.length; i++){
+//     divs[i].addEventListener('click', function() {
+//         console.log(this.getAttribute('id'))
+//     }, true) // на всплытие
+// }
