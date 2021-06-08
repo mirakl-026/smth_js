41e096d5e14d5a243b29f6951af3a9922711cc3a
Evgeniy Bushin <evgeniy.pgm@inbox.ru>
2021-06-08 13:08:38 +0300

    делегирование обработки событий

---
+++ 3_dom/9_delegateEvents.js
@@ -0,0 +1,31 @@
+// стандартный способ
+// var ps = document.querySelectorAll('p')
+
+// for (var i = 0; i < ps.length; i++) {
+//     ps[i].addEventListener('click', function(event){
+//         event.target.style.color = 'blue'
+//     })
+// }
+
+// А ЕСЛИ 10_000 раз????
+// то делегирование
+
+// прослушка события не на каждый элемент а на весь документ
+// var wrapper = document.querySelector('#wrapper')
+// но getElemntById быстрее
+
+document.getElementById('wrapper').addEventListener('click',function(event){
+    //console.log(event.target)
+
+    // меняем цвет только у параграфов
+    // console.log(event.target.tagName)
+    var tagName = event.target.tagName.toLowerCase()
+
+    if (tagName === 'p'){
+        event.target.style.color = 'blue'
+    }
+
+    if (event.target.classList.contains('color')){
+        event.target.style.color = 'red'
+    }
+})
