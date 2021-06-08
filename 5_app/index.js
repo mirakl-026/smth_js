a7f01ae7c37779131d22d730e61c3655b05878ba
Evgeniy Bushin <evgeniy.pgm@inbox.ru>
2021-06-08 15:28:57 +0300

    создание квадрата и обработчики

--- 5_app/index.js
+++ 5_app/index.js
@@ -3,7 +3,25 @@
 var $start = document.querySelector('#start')
 var $game = document.querySelector('#game')
 
+var score = 0
+
 $start.addEventListener('click', startGame)
+$game.addEventListener('click',handleBoxClick)
+
+/*
+    После нажатия на кнопку start - кнопка хайдится
+    поле (game) становится белым
+    - вызывается функция рендерБокс
+
+    в функции рендерБокс
+    - чистится поле game (удаляется предыдущий бокс)
+    - создаётся див - бокс с атрибутом box:true (дата- опускается)
+    - добавляется в поле
+
+    на поле повешен обработчик событий
+    - если кликнули в объект у которого box:true - счётчик попадаений score увеличивается
+    и запускается функция рендебокс заново
+*/
 
 function startGame() {
     //console.log('game started')
@@ -20,5 +38,32 @@ function startGame() {
 
 // функция рандомной генерации квадратов
 function renderBox () {
+    // очистка поля
+    $game.innerHTML = ''
 
+    var box = document.createElement('div')
+
+    box.style.height = box.style.width = '50px'
+    box.style.position = 'absolute' // позиция относительно квадрата в котором сгенерирован
+    box.style.backgroundColor = '#000'
+
+    box.style.top = '50px'
+    box.style.left = '70px'
+    box.style.cursor = 'pointer'
+    box.setAttribute('data-box','true') // добавляет без префикса
+
+    // помещение в #game (mdn)
+    $game.insertAdjacentElement('afterbegin', box)
+
+    // событие на блок
+
+}
+
+// если клиакают по квадрату
+function handleBoxClick(event) {
+    // проверка на box (data-box)
+    if (event.target.dataset.box) {    
+        score++
+        renderBox()
+    }
 }
