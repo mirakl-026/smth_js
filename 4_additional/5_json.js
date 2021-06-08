d8df510017f0d00b91933b1461ec7e00f8699796
Evgeniy Bushin <evgeniy.pgm@inbox.ru>
2021-06-08 14:03:58 +0300

    JSON

---
+++ 4_additional/5_json.js
@@ -0,0 +1,19 @@
+var person = {
+    name: 'Max',
+    age: 26,
+    job: 'GameDev',
+    friends: ['Elena', 'Igor'],
+    car: {
+        model: 'Ford'
+    }
+}
+
+// превращение в строку
+console.log(JSON.stringify(person))
+
+// парсинг в объект
+var str = JSON.stringify(person)
+console.log(JSON.parse(str))
+
+var personFrom = JSON.parse(str)
+console.log(personFrom.name, personFrom.friends)
