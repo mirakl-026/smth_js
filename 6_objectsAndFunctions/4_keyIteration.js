d60a88ad2d40a0511318abb38283c61f4533bc2f
Evgeniy Bushin <evgeniy.pgm@inbox.ru>
2021-06-09 14:27:29 +0300

    итерация по ключам

---
+++ 6_objectsAndFunctions/4_keyIteration.js
@@ -0,0 +1,19 @@
+// итерация по ключам
+
+var person = {
+    name: 'Max',
+    age: 28,
+    job: 'Frontend'
+}
+
+for (var key in person) {
+    if (person.hasOwnProperty(key)){
+        console.log(key, person[key])   // только собственные поля
+    }    
+}
+
+// 2 способ
+// возвращает массив ключей данного объекта
+Object.keys(person).forEach(function(key) {
+    console.log(person[key])
+})
