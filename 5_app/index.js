59575541e73c84d42ea1a3a4eaf6092ee3e87fe3
Evgeniy Bushin <evgeniy.pgm@inbox.ru>
2021-09-27 23:29:21 +0300

    .

--- 5_app/index.js
+++ 5_app/index.js
@@ -1,33 +1,33 @@
 
 // есл через QuerySelector - $ в названии
-var $start = document.querySelector('#start')
-var $game = document.querySelector('#game')
-var $time = document.querySelector('#time')
+var $start = document.querySelector("#start");
+var $game = document.querySelector("#game");
+var $time = document.querySelector("#time");
 
-var $result = document.querySelector('#result')
+var $result = document.querySelector("#result");
 
-var $time_header = document.querySelector('#time-header')
-var $result_header = document.querySelector('#result-header')
+var $time_header = document.querySelector("#time-header");
+var $result_header = document.querySelector("#result-header");
 
-var $gameTime = document.querySelector('#game-time')
+var $gameTime = document.querySelector("#game-time");
 
-var score = 0
-var isGameStarted = false
+var score = 0;
+var isGameStarted = false;
 
-$start.addEventListener('click', startGame)
-$game.addEventListener('click',handleBoxClick)
-$gameTime.addEventListener('input',setGameTime)
+$start.addEventListener("click", startGame);
+$game.addEventListener("click",handleBoxClick);
+$gameTime.addEventListener("input",setGameTime);
 
 
 
 
 
 function show($el) {
-    $el.classList.remove('hide')
+    $el.classList.remove("hide");
 }
 
 function hide ($el) {
-    $el.classList.add('hide')
+    $el.classList.add("hide");
 }
 
 
@@ -55,121 +55,121 @@ function hide ($el) {
 
 function startGame() {
 
-    score = 0
-    setGameTime()
+    score = 0;
+    setGameTime();
 
-    $gameTime.setAttribute('disabled','true')
+    $gameTime.setAttribute("disabled","true");
 
     // взводим флаг, что игра стартанула
     isGameStarted = true;
 
     // после нажанитя кнопки старт - кнопка должна пропасть
-    hide($start) // он есть в CSS
+    hide($start); // он есть в CSS
 
     // красим поле в белый цвет
-    $game.style.backgroundColor = 'white'
+    $game.style.backgroundColor = "white";
 
 
     var interval = setInterval(function() {
-        var time = parseFloat($time.textContent)
+        var time = parseFloat($time.textContent);
 
         if (time <= 0) {
             // end game
-            clearInterval(interval)
-            endGame()
-
-        } else {
-            $time.textContent = (time - 0.1).toFixed(1)
+            clearInterval(interval);
+            endGame();
+        } 
+        else {
+            $time.textContent = (time - 0.1).toFixed(1);
         }
-    }, 100) // каждые 100 мс
+    }, 100); // каждые 100 мс
 
     // генерация квадратов
-    renderBox()
+    renderBox();
 }
 
 
 
 
-function endGame(){
-    isGameStarted = false
-    setGameScore()
+function endGame() {
+    isGameStarted = false;
+    setGameScore();
 
     // вывод результата
-    show($start)
-    $game.innerHTML = ''        // удаляем боксы
-    $game.style.backgroundColor = '#ccc'
+    show($start);
+    $game.innerHTML = "";        // удаляем боксы
+    $game.style.backgroundColor = "#ccc";
 
-    hide($time_header)
-    show($result_header)
+    hide($time_header);
+    show($result_header);
     
-    $gameTime.removeAttribute('disabled')
+    $gameTime.removeAttribute("disabled");
 }
 
 
 function setGameTime () {
-    var time = +$gameTime.value
-    $time.textContent = (time).toFixed(1)
-    show($time_header)
-    hide($result_header)
+    var time = +$gameTime.value;
+    $time.textContent = (time).toFixed(1);
+    show($time_header);
+    hide($result_header);
 }
 
 
 function setGameScore () {
-    $result.textContent = score.toString()
+    $result.textContent = score.toString();
 }
 
 
 // функция рандомной генерации квадратов
 function renderBox () {
     // очистка поля
-    $game.innerHTML = ''
+    $game.innerHTML = "";
 
     //получение размера квадрата
-    var boxSize = getRandom(30,100)
+    var boxSize = getRandom(30,100);
 
     // получаем размер поля (для максимального топ и лефт)
-    var gameSize = $game.getBoundingClientRect()
-    var maxTop = gameSize.height - boxSize  // максимальное смещение по высоте для генерации квадрата 
-    var maxLeft = gameSize.width - boxSize  // максимальное смещение по левой стороне для генерации квадрата
+    var gameSize = $game.getBoundingClientRect();
+    var maxTop = gameSize.height - boxSize;  // максимальное смещение по высоте для генерации квадрата 
+    var maxLeft = gameSize.width - boxSize;  // максимальное смещение по левой стороне для генерации квадрата
 
-    var box = document.createElement('div')
+    var box = document.createElement("div");
 
-    box.style.height = box.style.width = boxSize + 'px'
-    box.style.position = 'absolute' // позиция относительно квадрата в котором сгенерирован
-    box.style.backgroundColor = getRandColor()
+    box.style.height = box.style.width = boxSize + "px";
+    box.style.position = "absolute"; // позиция относительно квадрата в котором сгенерирован
+    box.style.backgroundColor = getRandColor();
 
-    box.style.top = getRandom(0, maxTop) + 'px'
-    box.style.left = getRandom(0, maxLeft) + 'px'
-    box.style.cursor = 'pointer'
-    box.setAttribute('data-box','true') // добавляет без префикса
+    box.style.top = getRandom(0, maxTop) + "px";
+    box.style.left = getRandom(0, maxLeft) + "px";
+    box.style.cursor = "pointer";
+    box.setAttribute("data-box","true"); // добавляет без префикса
 
     // помещение в #game (mdn)
-    $game.insertAdjacentElement('afterbegin', box)
+    $game.insertAdjacentElement("afterbegin", box);
 }
 
 function getRandColor() {
-    var r = Math.ceil(Math.random() * 255)
-    var g = Math.ceil(Math.random() * 255)
-    var b = Math.ceil(Math.random() * 255)
+    var r = Math.ceil(Math.random() * 255);
+    var g = Math.ceil(Math.random() * 255);
+    var b = Math.ceil(Math.random() * 255);
 
     // rgb(28, 68, 99)
     
-    return 'rgb('+r+','+g+','+b+')'    
+    return "rgb("+r+","+g+","+b+")"    
 }
 
 // если клиакают по квадрату
 function handleBoxClick(event) {
     if (!isGameStarted) {
-        return
+        return;
     }
 
     // проверка на box (data-box)
     if (event.target.dataset.box) {    
-        score++
-        renderBox()
+        score++;
+        renderBox();
     }
 }
 
 function getRandom(min, max) {
-    return Math.floor(Math.random() * (max - min) + min)
+    return Math.floor(Math.random() * (max - min) + min);
 }
