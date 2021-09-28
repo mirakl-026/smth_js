d2bf81d8e57d3563153dacaecdd099de3373a786
Evgeniy Bushin <evgeniy.pgm@inbox.ru>
2021-09-28 21:30:50 +0300

    fixes

--- 2_base/5_operations2.js
+++ 2_base/5_operations2.js
@@ -16,7 +16,7 @@ console.log("i = i - 1:", i);
 i++;
 console.log("i++ :", i);
 i--;
-console.log("i++ :", i);
+console.log("i-- :", i);
 
 console.log("i++ (in arg):", i++);
 console.log("i-- (in arg):", i--);
@@ -25,7 +25,7 @@ console.log("++i (in arg):", ++i);
 console.log("--i (in arg):", --i);
 
 i += 16;
-console.log("i+= 3:", i);  // same to -, *, /
+console.log("i+= 16:", i);  // same to -, *, /
 
 i %= 5;
 console.log("i %= 5:", i);
--- 2_base/7_logical.js
+++ 2_base/7_logical.js
@@ -22,7 +22,7 @@ console.log("true || false:", true || false);
 console.log("false || false:", false || false);
 console.log("false || !false:", false || !false);
 
-console.log("(false && true || (true && false) || !true):", ((false && true) || (true || false) || !true));
+console.log("(false && true || (true || false) || !true):", ((false && true) || (true || false) || !true));
 
 // console.log(true | false);
 // console.log(true & false);
