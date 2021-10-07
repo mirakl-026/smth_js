fcbe10018cc8de5f8b9014f0a3c42096237676c5
Evgeniy Bushin <evgeniy.pgm@inbox.ru>
2021-10-07 20:41:59 +0300

    обработка POST запроса с помощью body-parser

--- 11_node_js/2/index.js
+++ 11_node_js/2/index.js
@@ -1,5 +1,6 @@
 // подключаем Express
 const express = require("express"); // без . и / - значит лезть в node_modules
+const bodyParser = require("body-parser");
 
 // объект приложения (сервера)
 const app = express();
@@ -7,11 +8,19 @@ const app = express();
 app.set('view engine', 'ejs');  // установка шаблонизатора
 
 // подключаем стили (статические файлы)
-app.use(express.static("public"))
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded( {extended : true} ));    // для расшифровки rquest body
 
 // чутка роутинга, чтобы не ловить ошибку
 app.get("/", (requset, response) =>{
     response.render('index');   // при запросе на корневую страницу - рендерим index.ejs
+});
+
+app.post("/", (req, res) => {
+    // console.log(req.body);    // смотрим, что в запросе
+    const { city } = req.body;
+    console.log(city);
+    res.render('index');
 });
 
 app.listen(3000, () =>{
