3b495649c9bf97671fc9a081be8046d8e7a8cce0
Evgeniy Bushin <evgeniy.pgm@inbox.ru>
2021-10-07 22:03:07 +0300

    вывод информации через шаблонизатор

--- 11_node_js/2/index.js
+++ 11_node_js/2/index.js
@@ -2,30 +2,33 @@
 const express = require("express"); // без . и / - значит лезть в node_modules
 const weatherRequest = require("./requests/weather.request");
 
+
 // объект приложения (сервера)
 const app = express();
 
 app.set('view engine', 'ejs');  // установка шаблонизатора
-
 // подключаем стили (статические файлы)
 app.use(express.static("public"));
 app.use(express.urlencoded());
 
+
+
 // чутка роутинга, чтобы не ловить ошибку
 app.get("/", (requset, response) =>{
-    response.render('index');   // при запросе на корневую страницу - рендерим index.ejs
+    response.render('index', {weather: null, error: null});   // при запросе на корневую страницу - рендерим index.ejs
 });
+
 
 app.post("/", async (req, res) => {
-    // console.log(req.body);    // смотрим, что в запросе
+    // вычленияем значение города из запроса
     const { city } = req.body;
-
     // получили город - запрос по API погоды и рендер
     const {weather, error} = await weatherRequest(city);
-    console.log("weather: ", weather);
-    console.log("error: ", error);
-    res.render('index');
+    res.render('index', {weather, error});
 });
+
+
+
 
 app.listen(3000, () =>{
     console.log("Server has started on port 3000...");
